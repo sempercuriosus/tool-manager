fcad807d0b5e4494d38f2c74ff04cd15069a6964
sempercuriosus <vespers-garages.0n@icloud.com>
2023-10-31 19:37:25 -0400

    updated the connection to use the .env variables

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,13 @@
+// require env file
+require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize (
-    'tool_library',
-    //User
-    'root',
-    //Password
-    '',
-   
-    //DB Location
-    host:'',
-    this.getDialect,
-    port: ''
-)
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        port: 3306
+    });
 
 module.exports = sequelize;
