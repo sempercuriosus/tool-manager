563cd837afd400297970b6ff42bac38e2969e736
Teej <tyrush1911@outlook.com>
2023-10-30 20:01:08 -0400

    connection updates

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,14 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize (
     'tool_library',
     //User
+    'root',
     //Password
+    '',
+   
     //DB Location
+    host:'',
+    this.getDialect,
+    port: ''
 )
 
 module.exports = sequelize;
