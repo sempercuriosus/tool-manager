cca3516823295bb5f81f3af192eeab5e25059ecd
sempercuriosus <vespers-garages.0n@icloud.com>
2023-11-01 20:52:06 -0400

    changed phrasing

--- controllers/index.js
+++ controllers/index.js
@@ -11,7 +11,7 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-    res.send("<h1>Route Not Valid!</h1><br/><br/><p>The route you are accessing is not defined and, therefore, invalid. </p>");
+    res.send("<h1>Route Not Valid!</h1><br/><br/><p>The route you are accessing is not defined and, therefore, is invalid. </p>");
 });
 
 module.exports = router;
