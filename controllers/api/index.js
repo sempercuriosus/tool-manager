afdcc9a3e1101639cf415d56cd2ae356fa1239da
Teej <tyrush1911@outlook.com>
2023-11-02 19:34:26 -0400

    index.js adds for square api (courtesy of Modeste)

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
+const express = require('express');
+const ejs = require('ejs');
+const request = require('request');
+const url = 'https://connect.squareup.com/v2/payments'
 
 // Routes
 const ping = require('./pong');
@@ -8,3 +12,31 @@ router.use('/ping', ping);
 // router.use('/users', userRoutes);
 
 module.exports = router;
+
+
+const data = {
+  "idempotency_key": "afba-ba953ba59b4a" + Date.now(),
+  "amount_money": {
+    "amount": 1000,
+    "currency": "USD"
+  },
+  "source_id": "EXTERNAL",
+  "external_details": {
+    "source": "Food Delivery Service",
+    "type": "OTHER"
+  }
+};
+const customHeaders = {
+    "Square-Version": "2023-10-18",
+    "Authorization": "Bearer EAAAEZz1co6Wm6iwTw_lFBPnGsNoL6BT4a7r9eycjk9UpAI9760DEykVD58Eyz7G",
+    "Content-Type": "application/json"
+}
+request({
+    url: url,
+    method: "POST",
+    headers: customHeaders,
+    json: true,   // <--Very important!!!
+    body: data
+}, function (error, response, body){
+    console.log(body);
+});
