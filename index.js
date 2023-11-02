bdf8ebce4fb0a05b9d960396d9a397d334d2db77
sempercuriosus <vespers-garages.0n@icloud.com>
2023-11-01 20:00:41 -0400

    reverted force on start

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 //     .then(() => {
 // console.info('Database connection has been established successfully.');
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
         console.info(`App listening on port ${PORT}!`);
     });
