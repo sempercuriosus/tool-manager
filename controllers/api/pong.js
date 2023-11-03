149f43c9f56f61ecf079c3f0595b4cd218ea739d
sempercuriosus <vespers-garages.0n@icloud.com>
2023-11-02 21:15:50 -0400

    added code comments

--- controllers/api/pong.js
+++ controllers/api/pong.js
@@ -1,8 +1,19 @@
+
+// require router
 const router = require('express').Router();
+
+// #region pong
+/*
+ * returns a pong message to check that this server is running and is rechable. 
+ * 
+*/
 
 router.use('/', (req, res) => {
     console.log('The user has pinged!');
     res.send('Therefore, I must pong...');
 });
 
+// #endregion pong
+
+// export
 module.exports = router;
