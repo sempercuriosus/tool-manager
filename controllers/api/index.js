859e8502c6abfc80161e2f9563fa540db7eb07d2
sempercuriosus <vespers-garages.0n@icloud.com>
2023-11-07 16:15:15 -0500

    removed comments

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -24,31 +24,3 @@ router.use('/users', userRoutes);
 router.use('/tools', toolRoutes);
 
 module.exports = router;
-
-
-// const data = {
-//   "idempotency_key": "afba-ba953ba59b4a" + Date.now(),
-//   "amount_money": {
-//     "amount": 1000,
-//     "currency": "USD"
-//   },
-//   "source_id": "EXTERNAL",
-//   "external_details": {
-//     "source": "Food Delivery Service",
-//     "type": "OTHER"
-//   }
-// };
-// const customHeaders = {
-//     "Square-Version": "2023-10-18",
-//     "Authorization": "Bearer EAAAEZz1co6Wm6iwTw_lFBPnGsNoL6BT4a7r9eycjk9UpAI9760DEykVD58Eyz7G",
-//     "Content-Type": "application/json"
-// }
-// request({
-//     url: url,
-//     method: "POST",
-//     headers: customHeaders,
-//     json: true,   // <--Very important!!!
-//     body: data
-// }, function (error, response, body){
-//     console.log(body);
-// });
