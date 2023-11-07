1fe300935f29413a313872e02f4ad40ea9c12d6c
sempercuriosus <vespers-garages.0n@icloud.com>
2023-11-06 21:44:18 -0500

    added redirect to checked out route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -12,10 +12,16 @@
 
 // Router Instance
 const router = require('express').Router();
-// Model
-const { User } = require('../models');
+
 // Auth
 const withAuth = require('../utils/auth');
+
+// Models
+const { User } = require('../models');
+
+// Sequelize
+const sequelize = require('../config/connection');
+
 
 /*
   * Main Route
@@ -25,7 +31,8 @@ router.get('/', withAuth, async (req, res) => {
   // Render the profile partial 
   try {
     // console.log('DO ---------> render profile');
-    res.status(200).render('homepage', { pageTitle: 'User Profile', userData: 'USER', layout: 'toolDirectory' });
+    // res.status(200).render('homepage', { pageTitle: 'User Profile', layout: 'profile' });
+    res.redirect('/checkedout');
   }
   catch {
     res.status(500).json({ 'result': 'There was an error in rendering the Profile.' });
@@ -42,7 +49,7 @@ router.get('/login', (req, res) => {
   // DO redirect to the root route to the homepage
   if (req.session.logged_in) {
     // console.log('DO ---------> user is logged in, REDIRECT to main');
-    return res.status(204).redirect('/');
+    return res.redirect('/');
   }
 
   // If this is executed then the user IS NOT logged in and needs to be sent to the login page.
@@ -52,6 +59,42 @@ router.get('/login', (req, res) => {
   }
   catch {
     console.error('Could not Render the login page');
+  }
+});
+
+
+/*
+  * Logout
+  * 
+  * 
+*/
+router.get('/checkedout', async (req, res) => {
+  // Get the available tools 
+
+  const userId = req.session.user_id;
+  const userRentals = await sequelize.query(
+    `
+    SELECT t.tool_name, tco.checkout_date, tco.return_date
+    FROM tools_checked_out tco
+    INNER JOIN tools t ON tco.tool_id = t.id
+    WHERE tco.user_id = :userId
+    `,
+    {
+      replacements: { userId },
+      type: sequelize.QueryTypes.SELECT,
+    }
+  );
+
+  console.log("Checked out");
+  if (req.session.logged_in) {
+    res.render('homepage', { rentals: userRentals, pageTitle: 'Profile', userData: 'USER', layout: 'profile' });
+  }
+});
+
+router.get('/available', (req, res) => {
+  // Get the available tools 
+  if (req.session.logged_in) {
+    res.render('homepage', { pageTitle: 'Tools', userData: 'USER', layout: 'toolDirectory' });
   }
 });
 
